0bb4f72a755dcf09bdd4ba3bb6535ae53b572893
piguchi@geolonia.com <piguchi@geolonia.com>
2025-08-01 09:12:16 +0900

    国土数値情報の出典の取得を削除し、attributionを追加

--- docs/utils/nationalLandNumericalInformationUtils.d.ts
+++ docs/utils/nationalLandNumericalInformationUtils.d.ts
@@ -2,4 +2,3 @@ export declare const addNLNISource: (map: maplibregl.Map, nlniId: string) => str
 export declare const addNLNILayer: (map: maplibregl.Map, key: string) => void;
 export declare const removeNLNILayer: (map: maplibregl.Map, key: string) => void;
 export declare function getNLNIKeys(): string[];
-export declare function getNLNIInfos(keys: string[]): string[];
