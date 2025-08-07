591742a93cb3cba21e3e5b089d55c38a99ff6271
piguchi@geolonia.com <piguchi@geolonia.com>
2025-08-07 09:43:45 +0900

    osmレイヤーが重なっても出るように修正

--- docs/utils/spriteUtils.d.ts
+++ docs/utils/spriteUtils.d.ts
@@ -1,4 +1,10 @@
 /**
+ * 指定のスプライトシートに含まれるアイコン名一覧を取得する
+ * @param spriteSheetUrl スプライトシートのURL（例: https://geolonia.github.io/chizubouken-lab-sprite/sprite）
+ * @returns Promise<string[]> アイコン名の配列
+ */
+export declare function getSpriteIconNames(spriteSheetUrl: string): Promise<string[]>;
+/**
  * 指定のスプライトシートに指定アイコン名が存在するかどうかを確認する
  * @param spriteSheetUrl スプライトシートのURL（例: https://geolonia.github.io/chizubouken-lab-sprite/sprite）
  * @param iconName アイコン名（例: "pin"）
